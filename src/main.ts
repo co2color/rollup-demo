4d2239045793cf1dce9f3d362406a07f1838c81f
co2color <819109255@qq.com>
2024-03-22 22:18:22 +0800

    feat

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { isNumber } from 'lodash-es'
 
-const app = document.getElementById('#app')
+const app = document.getElementById('app')
 setTimeout(() => {
   if (app)
     app.innerHTML = isNumber(1) ? '1 is a number' : '1 is not a number'
